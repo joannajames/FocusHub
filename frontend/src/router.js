79fa54f087005298c8221a425ecfab59d41224aa
dho-rae <raeesa.dhoda@gmail.com>
2025-03-03 13:36:00 -0500

    Create router.js

---
+++ frontend/src/router.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '@/components/HomePage.vue';
+import AccountPage from '@/components/AccountPage.vue';
+import ReviewsPagePage from '@/components/ReviewsPage.vue';
+import TheHubPagePage from '@/components/TheHubPage.vue';
+import ContactUsPage from '@/components/ContactUsPage.vue';
+
+const routes = [
+  { path: '/', component: HomePage },
+  { path: '/account', component: AccountPage },
+  { path: '/reviews', component: ReviewsPagePage },
+  { path: '/the_hub', component: TheHubPagePage },
+  { path: '/contact_us', component: ContactUsPage }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
