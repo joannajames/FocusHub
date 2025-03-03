f855067751a6d6dc2af41aa9e1e92fd23a705a71
dho-rae <raeesa.dhoda@gmail.com>
2025-03-03 13:35:38 -0500

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-view/>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App'
+});
+</script>
+
+<style>
+@import '@fontsource/rubik-doodle-shadow';
+
+body {
+  background: #fdfde3;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  width: 100vw;
+  box-sizing: border-box;
+}
+</style>
