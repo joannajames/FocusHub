0ab627b37e7c10bc2cc4f7dec1175e28f4fb24b8
Raeesa Dhoda <raeesa.dhoda@gmail.com>
2025-04-15 17:22:18 -0400

    Page for redirecting non-logged in users

---
+++ frontend/src/components/NotVisible.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="page-frame">
+    <div class="container">
+      <header class="header">
+          <div class="nav-container">
+          <img src="/icons/Menu_Burger.png" alt="Menu" class="menu-icon" @click="toggleDropdown"/>
+            <ul class="dropdown" v-show="showDropdown">
+              <li>
+                <a href="#" @click="navigateTo('/the_hub')">
+                  -&nbsp;&nbsp;&nbsp;the&nbsp;&nbsp;Hub
+                  <img src="/icons/Hub_Stars.png" class="dropdown-icon" alt="the Hub Icon" />
+                </a>
+              </li>
+              <li>
+                <a href="#" @click="navigateTo('/profile')">
+                  -&nbsp;&nbsp;&nbsp;profile
+                  <img src="/icons/Account.png" class="dropdown-icon" alt="Profile Icon" />
+                </a>
+              </li>
+              <li>
+                <a href="#" @click="navigateTo('/chat_to_us')">
+                  -&nbsp;&nbsp;&nbsp;chat&nbsp;&nbsp;to&nbsp;&nbsp;us
+                  <img src="/icons/Chat_To_Us.png" class="dropdown-icon" alt="Chat to Us Icon" />
+                </a>
+              </li>
+              <li>
+                <a href="#" @click="navigateTo('/our_constitution')">
+                  -&nbsp;&nbsp;&nbsp;our&nbsp;&nbsp;constitution
+                  <img src="/icons/Scroll.png" class="dropdown-icon" alt="Constitution Icon" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        <div class="nav-section">
+          <img src="/icons/Account.png" alt="Profile Icon" class="login-icon" @click="navigateTo('/profile')" />
+          <img src="/icons/FocusHub_Logo.png" alt="FocusHub Logo" class="logo-icon" @click="navigateTo('/')" />
+        </div>
+      </header>
+
+      <div class="main-content left-margin">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <p> Error: login with your BU email to </p>
+        <p> access this page </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const showDropdown = ref(false);
+
+const toggleDropdown = () => {
+  showDropdown.value = !showDropdown.value;
+};
+
+const navigateTo = (path) => {
+  window.location.href = path;
+  showDropdown.value = false;
+};
+</script>
+
+<style scoped>
+
+p {
+  font-size: 50px;
+  line-height: 0.9;
+  word-spacing: 4px;
+  letter-spacing: 1px;
+  font-family: 'Victor Mono Variable', serif;
+}
+
+</style>
