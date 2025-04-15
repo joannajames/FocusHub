d4ede730023a9ba7766f55cfb4160874abe0f970
Raeesa Dhoda <raeesa.dhoda@gmail.com>
2025-04-15 10:58:40 -0400

    Update router.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,16 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '@/components/HomePage.vue';
-import AccountPage from '@/components/AccountPage.vue';
-import ReviewsPagePage from '@/components/ReviewsPage.vue';
-import TheHubPagePage from '@/components/TheHubPage.vue';
-import ContactUsPage from '@/components/ContactUsPage.vue';
+import UserProfile from '@/components/UserProfile.vue';
+import DynamicReviews from '@/components/DynamicReviews.vue';
+import TheHub from '@/components/TheHub.vue';
+import ChatToUs from '@/components/ChatToUs.vue';
+import OurConstitution from "@/components/OurConstitution.vue";
+import NotVisible from "@/components/NotVisible.vue";
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/account', component: AccountPage },
-  { path: '/reviews', component: ReviewsPagePage },
-  { path: '/the_hub', component: TheHubPagePage },
-  { path: '/contact_us', component: ContactUsPage }
+  { path: '/profile', component: UserProfile },
+  { path: '/reviews/:id', component: DynamicReviews },
+  { path: '/the_hub', component: TheHub },
+  { path: '/chat_to_us', component: ChatToUs },
+  { path: '/our_constitution', component: OurConstitution },
+  { path: '/unavailable', component: NotVisible }
 ];
 
 const router = createRouter({
